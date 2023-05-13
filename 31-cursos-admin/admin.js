8ce2764bf21c62ff831df6bc8b4723cb18885691
Eduardo Trigo <eduatrigo@gmail.com>
2023-05-13 17:34:14 -0300

    agregar carpeta cursos

---
+++ 31-cursos-admin/admin.js
@@ -0,0 +1,24 @@
+class Cursos {
+    id
+    nombre
+    descripcion
+    publicado
+
+    constructor(id, nombre, descripcion, publicado){
+        this.id = id
+        this.nombre = nombre
+        this.descripcion = descripcion
+        this.publicado = publicado
+    }
+}
+
+class ColeccionCursos{
+    cursos
+    constructor(){
+        this.cursos = []
+    }
+}
+
+const curso1 = new ColeccionCursos()
+const cursoJavascript = new Cursos(1 , "javascript" , "curso javascript", true)
+const cursoHtmlCss = new Cursos(1 , "Html Css" , "curso Html y Css", false)
