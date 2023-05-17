51909f8956a4aa01604bd9e7897b89df4db0796c
Eduardo Trigo <eduatrigo@gmail.com>
2023-05-17 20:40:25 -0300

    modificaciones

--- 035-mi-primer-app-react/src/App.jsx
+++ 035-mi-primer-app-react/src/App.jsx
@@ -1,9 +1,26 @@
 import './App.css'
 
 function App() {
+  const nombre = "Eduardo"
+  const anios = 38
+  const arregloNombres = ["juan", "daniela", "sebastian", "jose", "pedro"]
   return (
     <>
-      <h1>Eduardo Trigo</h1>
+    <div className='bg-dark text-white p-5 rounded'>
+      <h1 className='text-danger'>Eduardo Trigo</h1>
+      <h2>este es un subtitulo</h2>
+      <p>hola mi nombre es {nombre}</p>
+      <p>tengo {anios} años</p>
+      <ul>
+        {
+          arregloNombres.map(function (item, idx) {
+             return <li className='text-warning' key={idx}>
+              {item}
+             </li>
+          })          
+        }
+      </ul>
+    </div>
     </>
   )
 }
