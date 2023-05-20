c5cb1091a6b0445d0ae6e7f3fa2688c22da3ea85
Eduardo Trigo <eduatrigo@gmail.com>
2023-05-20 00:12:51 -0300

    modificaciones contador

--- counterApp/src/counterApp.jsx
+++ counterApp/src/counterApp.jsx
@@ -1,20 +1,49 @@
-import { useState } from"react"
+import { useState } from "react"
 
-function CounterApp(){
-    const [count, setCount]= useState(0)
+function CounterApp() {
+    //hucks---------------------------------------
+    const [count, setCount] = useState(0)
+    const [valorInput, setValorInput] = useState("")
 
+    //funciones-----------------------------------
+    const changeInput = (event) => {
+        setValorInput(event.target.value)
+    }
+
+    const sumarUno = () => {
+        setCount(count + 1)
+    }
+
+    const restarUno = () => {
+        setCount(count - 1)
+    }
+
+    const resetear = () => {
+        setCount(0)
+    }
     return (
         <>
-        <div className="text-white text-center">
-            <h1 className="my-5">CounterApp</h1>
-            <p>tengo un contador</p>
-            <p>{count}</p>
-            <div className="text center  ">
-                <button className="btn btn-primary" onClick={() => {setCount( count + 1)}}>sumar+1</button>
-                <button className="btn btn-danger mx-3" onClick={() => {setCount( count - 1)}}>restar-1</button>
-                <button className="btn btn-warning" onClick={() => {setCount( count = 0)}}>reiniciar 0</button>
+            <div className="text-white text-center">
+                <h1 className="my-5">CounterApp</h1>
+                <p>tengo un contador</p>
+                <p>{count}</p>
+                <div className="text center ">
+                    <button className="btn btn-primary" onClick={sumarUno}>sumar+1</button>
+                    <button className="btn btn-danger mx-3" onClick={restarUno}>restar-1</button>
+                    <button className="btn btn-warning" onClick={resetear}>reiniciar 0</button>
+                </div>
+                <hr className="text-white my-5" />
+                <div className=" w-50 mx-auto " >
+                    <label className="my-2">input</label>
+                    <input
+                        type="text "
+                        className="form-control"
+                        placeholder="Ej: asdf ñlkj"
+                        name="name"
+                        value={valorInput}
+                        onChange={changeInput} />
+                </div>
             </div>
-        </div>
         </>
     )
 }
